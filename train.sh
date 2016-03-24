eee89f910aea15156ef750ca901448b766427e26
Nehal J Wani <nehaljw.kkd1@gmail.com>
2016-03-24 20:01:19 +0530

    Provide only token,tag for training

--- train.sh
+++ train.sh
@@ -6,6 +6,6 @@ fi
 INPUT_FILE_SSF=$1
 MODEL_NAME=$2
 TRAINING_FILE="./run/training.in_$RANDOM"
-cat $INPUT_FILE_SSF | perl ./utils/ssf2tnt.pl | perl ./utils/extra_features.pl > $TRAINING_FILE
+cat $INPUT_FILE_SSF | perl ./utils/ssf2tnt.pl | awk '{print $1, $2}' | perl ./utils/extra_features.pl > $TRAINING_FILE
 crf_learn template $TRAINING_FILE ${MODEL_NAME}.model
 rm -f $TRAINING_FILE
